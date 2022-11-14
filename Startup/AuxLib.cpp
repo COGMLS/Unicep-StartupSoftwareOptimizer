27e48e84cdb8bc01a2965cc7fff2ecb2a2b24b0d
COGMLS <mlsmatheus@hotmail.com>
2022-11-14 19:48:47 -0300

    Add readConfig, chkFile and chkItem functions

--- Startup/AuxLib.cpp
+++ Startup/AuxLib.cpp
@@ -36,3 +36,77 @@ int getEnv(std::string& envPath, const char* varName)
 	
 	return 0;	// The environment variable was founded and exported with success!
 }
+
+int readConfig(std::fstream& fs, const char* token, std::string& config)
+{
+	if (fs.is_open())
+	{
+		if (fs.is_open())
+		{
+			if (fs.tellg() != 0)
+			{
+				fs.clear();
+				fs.seekg(0, std::ios::beg);
+			}
+
+			std::string temp, temp2;
+			while (std::getline(fs, temp))
+			{
+				for (int j = 0; j < temp.size(); j++)
+				{
+					temp2 += temp[j];
+
+					if (std::strcmp(token, temp2.c_str()) == 0)
+					{
+						temp.replace(0, std::strlen(token), "");
+						config = temp;
+						return 0;		// Configuration founded and exported
+					}
+				}
+
+				temp2.clear();
+			}
+		}
+
+		return 2;	// Configuration not founded
+	}
+
+	return 1;		// Config file not available
+}
+
+int chkFile(std::filesystem::path path, std::filesystem::path filePath)
+{
+	if (chkItem(path, filePath))
+	{
+		return 0;	// File exist
+	}
+	else
+	{
+		std::fstream fs;
+		fs.open(filePath, std::ios::out);
+		fs.close();
+
+		if (chkItem(path, filePath))
+		{
+			return 1;	// File created
+		}
+		else
+		{
+			return -1;	// Fail to create the file
+		}
+	}
+}
+
+bool chkItem(std::filesystem::path path, std::filesystem::path item)
+{
+	std::filesystem::recursive_directory_iterator r(path);
+	std::filesystem::directory_entry d(item);
+	for (const std::filesystem::directory_entry& entry : r)
+	{
+		if (entry == d)
+		{
+			return true;
+		}
+	}
+	return false;
+}
--- Startup/AuxLib.hpp
+++ Startup/AuxLib.hpp
@@ -5,7 +5,12 @@
 
 #include <cstdlib>
 #include <string>
+#include <fstream>
+#include <filesystem>
 
 int getEnv(std::string& envPath, const char* varName);
+int readConfig(std::fstream& fs, const char* token, std::string& config);
+int chkFile(std::filesystem::path path, std::filesystem::path filePath);
+bool chkItem(std::filesystem::path path, std::filesystem::path item);
 
 #endif // !AUXLIB_HPP
